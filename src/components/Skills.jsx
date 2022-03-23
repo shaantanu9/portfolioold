0d3e9bc190b71655fc9a264f9a1ecadefd374229
shantanu <shantanubombatkar2@gmail.com>
2022-03-23 10:57:47 +0530

    Changes Done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,7 +17,7 @@ return(
       <p className="mx-4 my-2 font-semibold text-center text-gray-600">The future belongs to those who learn more skills and combine them in creative ways. ― Robert Greene</p>
       <div class="block lg:justify-between lg:flex lg:item-center">
       
-      <div className="mx-10 lg:ml-32"><a href="#skills"><iframe width="300" height="300"  title="skill" href="#skills" src="https://giphy.com/embed/4xyQcPmh44YiiZEEuZ" frameBorder="0" className="mr-2 ml-2 giphy-embed" allowFullScreen></iframe></a></div>
+      <div className="mx-10 lg:ml-40"><a href="#skills"><iframe width="300" height="300"  title="skill" href="#skills" src="https://giphy.com/embed/4xyQcPmh44YiiZEEuZ" frameBorder="0" className="mr-2 ml-2 giphy-embed" allowFullScreen></iframe></a></div>
       
     <div className="grid grid-cols-3 justify-between mt-8 ml-2 lg:mr-24 lg:ml-32 lg:grid-cols-6"> 
       <SkillImg src="https://sathish-portfolio.vercel.app/images/icons8-html-5.svg" alt="HTML5" title="HTML5" />
